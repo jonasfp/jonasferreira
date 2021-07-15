1c1b59d05984b7f3111af039d1f05fb6ccc63d5d
unknown <jonaspaixao@gmail.com>
2021-07-15 20:20:50 -0300

    Praticando

--- Pratica_02/src/HelloWorld.java
+++ Pratica_02/src/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 		System.out.println("Hello World");
 		System.out.println("Hello World - 1º Commit");
 		System.out.println("Comitando na branch master+hotfix");
+		System.out.println("Praticando");
 	}
 }
