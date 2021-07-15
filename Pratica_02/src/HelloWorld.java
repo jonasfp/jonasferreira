75249d69ec64c25ac2b00babfe376548bff24d13
Jonas <jonaspaixao@gmail.com>
2021-07-15 16:53:26 -0300

    Comitando na branch hotfix

--- Pratica_02/src/HelloWorld.java
+++ Pratica_02/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hello World - 1º Commit");
+		System.out.println("Comitando na branch hotfix");
 
 	}
 
